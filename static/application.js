22eda99c5beb372407b6861a1101887bd26ca7ca
Olivia Cheng <olivia@heyitsolivia.com>
2014-01-25 12:49:55 -0500

    remove Chart.js

--- static/application.js
+++ static/application.js
@@ -1,46 +1,13 @@
 $(function (){
 
-    /*// chart.js
-    var ctx = document.getElementById("sotu-chart").getContext("2d");
-    var data = [
-        {
-            value: 30,
-            color:"#F7464A"
-        },
-        {
-            value : 50,
-            color : "#E2EAE9"
-        },
-        {
-            value : 100,
-            color : "#D4CCC5"
-        },
-        {
-            value : 40,
-            color : "#949FB1"
-        },
-        {
-            value : 120,
-            color : "#4D5360"
-        }
-
-    ]
-    var options = {
-        segmentShowStroke : false,
-        percentageInnerCutout : 80
-    }
-    
-    new Chart(ctx).Doughnut(data, options);
-*/
-
     // gradient for slider fill
         
-    function fillSlider(e) {
-            var val = ($(e).val() - $(e).attr('min')) / ($(e).attr('max') - $(e).attr('min'));
+    function fillSlider() {
+            var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
             var fillColor = '#c9a706';
             var baseColor = '#333';
 
-            $(e).css('background-image',
+            $(this).css('background-image',
                         '-webkit-gradient(linear, left top, right top, '
                         + 'color-stop(' + val + ',' + fillColor + '),' 
                         + 'color-stop(' + val + ',' + baseColor + ')'
@@ -48,13 +15,9 @@ $(function (){
                         );
     }
 
-    $('input[type="range"]').each(function () {    
-        fillSlider(this);
-    });
-
-    $('input[type="range"]').change(function () {
-        fillSlider(this);
-    });
+    $('input[type="range"]')
+        .each(fillSlider)
+        .change(fillSlider);
 
     // buttons do things
     var $btncta = $('.btn-cta'),
